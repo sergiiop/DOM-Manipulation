55f4af35bc8a166ad0e685f2a42b1fd1fa0b2333
Sergio Pérez <sergio-luis98@hotmail.com>
2022-09-10 13:33:19 -0500

    typo

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ import router from './src/routes'
 import './style.css'
 import './src/index.css'
 
+console.log('hello')
+
 window.addEventListener('load', router)
 window.addEventListener('hashchange', router)
